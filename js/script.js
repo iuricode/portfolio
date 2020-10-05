31d84e5765e357413d288a7af7640ac3a7d81ac9
Iuri Silva <iuricold99@gmail.com>
2020-10-04 23:07:38 -0300

    colocando script do dark mode

--- js/script.js
+++ js/script.js
@@ -84,3 +84,42 @@ $(document).ready(function () {
 
 });
 
+//Dark Mode
+var darkMode;
+
+if (localStorage.getItem('dark-mode')) {
+    // if dark mode is not in storage, set variable to 'light'
+    darkMode = 'light';
+} else {
+    // if dark mode is in storage, set variable with that value
+    darkMode = localStorage.getItem('dark-mode');
+}
+
+// set new localStorage value
+localStorage.setItem('dark-mode', darkMode);
+
+if (localStorage.getItem('dark-mode') == 'dark') {
+    // if the above is 'dark' then apply .dark to the body
+    $('body').addClass('dark');
+    // hide the 'dark' button
+    $('.dark-button').hide();
+    // show the 'light' button
+    $('.light-button').show();
+}
+
+// Toggle dark UI
+$('.dark-button').on('click', function () {
+    $('.dark-button').hide();
+    $('.light-button').show();
+    $('body').addClass('dark');
+    // set stored value to 'dark'
+    localStorage.setItem('dark-mode', 'dark');
+});
+
+$('.light-button').on('click', function () {
+    $('.light-button').hide();
+    $('.dark-button').show();
+    $('body').removeClass('dark');
+    // set stored value to 'light'
+    localStorage.setItem('dark-mode', 'light');
+});
