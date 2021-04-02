77aa293e25d86d16784ffc31822ce28045315672
iuricode <iuricold99@gmail.com>
2021-04-02 18:06:19 -0300

    colocando animações

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,75 @@
 //Menu
 class MobileNavbar{
-            constructor(mobileMenu, navList, navLinks){
-                this.mobileMenu = document.querySelector(mobileMenu);
-                this.navList = document.querySelector(navList);
-                this.navLinks = document.querySelectorAll(navLinks);
-                this.activeClass = 'active';
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = 'active';
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-                this.handleClick = this.handleClick.bind(this);
-            }
+    animateLinks(){
+        this.navLinks.forEach((link, index) => {  
+        link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
+        });
+    }
 
-            animateLinks(){
-                this.navLinks.forEach((link, index) => {
-                
-                    link.style.animation
-                        ? (link.style.animation = "")
-                        : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
-                });
-            }
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
 
-            handleClick(){
-                this.navList.classList.toggle(this.activeClass);
-                this.mobileMenu.classList.toggle(this.activeClass);
-                this.animateLinks();
-            }
+    addClickEvent(){
+        this.mobileMenu.addEventListener('click', this.handleClick);
+    }
 
-            addClickEvent(){
-                this.mobileMenu.addEventListener('click', this.handleClick);
-            }
-
-            init(){
-                if(this.mobileMenu){
-                    this.addClickEvent();
-                }
-                return this;
-            }
+    init(){
+        if(this.mobileMenu){
+            this.addClickEvent();
         }
+        return this;
+    }
+}
+const mobileNavbar = new MobileNavbar(
+    '.mobile-menu',
+    '.nav-list',
+    '.nav-list li',
+);
+mobileNavbar.init();
 
-        const mobileNavbar = new MobileNavbar(
-            '.mobile-menu',
-            '.nav-list',
-            '.nav-list li',
-        );
-        mobileNavbar.init();
+//Maquina de escrever
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+              
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 100 * i)
+        
+    });
+}
+const titulo = document.querySelector('.maquina-escrever');
+typeWrite(titulo);
 
-//Scroll top
+//Scroll TOP
 $(document).ready(function () {
     var scrollTop = $('.scrollTop');
 
     $(window).scroll(function () {
         var topPos = $(this).scrollTop();
-
-        //quantidade Y que o botão de scroll vai aparecer
+        //Quantidade Y que o botão de scroll vai aparecer
         if (topPos > 500) {
             $(scrollTop).css('opacity', '1');
         } else {
             $(scrollTop).css('opacity', '0');
         }
     });
-    //velocidade a animação
+
+    //Velocidade a animação
     $(scrollTop).click(function () {
         $('html, body').animate(
             {
@@ -67,37 +79,59 @@ $(document).ready(function () {
         );
         return false;
     });
-    //Links dos meus conhecimentos
-    const techs = [{
-        name: 'htmlecss',
-        url: 'https://www.treinaweb.com.br/blog/o-que-e-e-como-comecar-com-html-e-css/'
-    }, {
-        name: 'js',
-        url: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/',
-    }, {
-        name: 'sass',
-        url: 'https://sass-lang.com/'
-    }, {
-        name: 'bootstrap',
-        url: 'https://getbootstrap.com/',
-    }, {
-        name: 'reactjs',
-        url: 'https://pt-br.reactjs.org/',
-    }, {
-        name: 'figma',
-        url: 'https://www.figma.com/'
-    }]
-
-    techs.forEach(tech => {
-        $('.' + tech.name).on('click', () => {
-            window.open(tech.url)
-        })
-    });
-
 });
+
+//Links dos meus conhecimentos
+const techs = [{
+    name: 'htmlecss',
+    url: 'https://www.treinaweb.com.br/blog/o-que-e-e-como-comecar-com-html-e-css/'
+}, {
+    name: 'js',
+    url: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/',
+}, {
+    name: 'sass',
+    url: 'https://sass-lang.com/'
+}, {
+    name: 'bootstrap',
+    url: 'https://getbootstrap.com/',
+}, {
+    name: 'reactjs',
+    url: 'https://pt-br.reactjs.org/',
+}, {
+    name: 'figma',
+    url: 'https://www.figma.com/'
+}]
+
+techs.forEach(tech => {
+    $('.' + tech.name).on('click', () => {
+        window.open(tech.url)
+    })
+});
+
+//Revelar animação
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+//Home delay
+sr.reveal('.button',{delay: 200})
+sr.reveal('.imagem-espaco-delay',{delay: 200})
+sr.reveal('.imagem-iuri-delay',{delay: 600})
+//Sobre mim delay
+sr.reveal('.imagem-delay',{})
+sr.reveal('.titulo-delay',{delay: 200})
+sr.reveal('.descricao-delay',{delay: 400})
+sr.reveal('.cards-interval',{interval: 400})
+
+
+
 
 
 /*
+//Validação captcha do contato
 valor = document.getElementById('campo').value
 
 function testar() {
