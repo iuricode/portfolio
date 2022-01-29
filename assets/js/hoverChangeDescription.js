fe8ab9ebb90c032a1b704f54cf97f018b5399071
Iuri Silva <iuricold99@gmail.com>
2022-01-29 19:00:10 -0300

    Update and rename writeDescription.js to hoverChangeDescription.js

---
+++ assets/js/hoverChangeDescription.js
@@ -0,0 +1,44 @@
+export function hoverChangeDescription(nameCard, text) {
+  var changeDescription = document.querySelector(".changeDescription");
+
+  document.querySelector(nameCard).addEventListener("mouseover", () => {
+    changeDescription.innerHTML = text;
+  });
+
+  document.querySelector(nameCard).addEventListener("mouseout", () => {
+    changeDescription.innerHTML = `*passe o cursor do mouse no card para ler*`;
+  });
+}
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas HTML."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma."
+);
+hoverChangeDescription(
+  ".sass",
+  "Sass (pré-processador css) é uma linguagem de folhas de estilo. É uma simples linguagem de script usada em arquivos Sass."
+);
+hoverChangeDescription(
+  ".react",
+  "ReactJS é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web."
+);
+hoverChangeDescription(
+  ".next",
+  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
+);
+hoverChangeDescription(
+  ".styled",
+  "Styled Components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "Tailwind CSS é um framework desenvolvido para maximizar o potencial do bom e velho CSS e levá-lo ainda mais longe."
+);
